32cd716c3b5c40e9b8d28c035f22b34df3b7f04e
Oluwa-Femi <oluwatolablessing@gmail.com>
2020-05-31 11:09:57 +0100

    Current update

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,27 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, { useState } from "react";
 import BottomRow from "./BottomRow";
 import "./App.css";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  const [score, setScore] = useState(32);
+
+  const incrementAway = () => {
+    setScore(score + 1);
+  };
+
+  const decrementAway = () => {
+    setScore(score + 1);
+  };
+
+  const incrementHome = () => {
+    setScore(score + 1);
+  };
+
+  const decrementHome = () => {
+    setScore(score + 1);
+  };
 
   return (
     <div className="container">
@@ -19,7 +36,7 @@ function App() {
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+  <div className="away__score">32</div>
           </div>
         </div>
         <BottomRow />
